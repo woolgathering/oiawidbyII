e7bd3ba5113b844fde33f37cbe6edd34a4441be6
Jacob Sundstrom <jacob.sundstrom@gmail.com>
2018-03-30 11:11:11 -0700

    Initial commit.

---
+++ loop.py
@@ -0,0 +1,71 @@
+import mne
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+f = "/home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11.edf"
+annotation = "home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11.txt"
+exclude = ["ROC-LOC", "ECG1-ECG2", "DX1-DX2", "SX1-SX2", "SAO2", "HR", "PLETH", "STAT"] # don't use these channels
+bands = [(7,45), (48,78), (80,123), (125,150), (153,248), (250,348), (350,448)]
+output = open("/home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11_analysis", "w")
+
+def normalize_complex_arr(a):
+  a_oo = a - a.real.min() - 1j*a.imag.min() # origin offsetted
+  return a_oo/np.abs(a_oo).max() # divide by the max magnitude
+
+raw = mne.io.read_raw_edf(f, exclude=exclude)
+# raw.plot(block=True, lowpass=40)
+
+overlap = 0.5
+seg_length = int(raw.info['sfreq'] * 2)
+advance = seg_length * overlap
+num_segs = int(raw.n_times/advance) # number of samples to advance per loop
+channel = 0 # what channel are we looking at?
+
+for seg in range(num_segs):
+  start = int(seg * advance) # get the staring sample
+  end = int(start + seg_length) # get the ending sample
+  time = (start + advance) / raw.info['sfreq'] # get the start time of the current frame
+
+  print ("\nStarting segment {}...\n".format(seg)) # tell us where we are
+
+  # get the raw values into their own arary
+  data = raw.get_data([channel], start, end) # extact the data from the channel
+  pad = (5120-len(data[0])) # get how much padding we need
+  data = np.lib.pad(data, pad_width=(0,pad), mode='constant', constant_values=0) # pad it
+  data = mne.filter.filter_data(data, raw.info['sfreq'], l_freq=None, h_freq=50, fir_design='firwin') # filter
+
+  fft = np.fft.fft(data) # fft
+  fft = normalize_complex_arr(fft) # normalize
+  x_axis = np.fft.fftfreq(len(data), 1/raw.info['sfreq']) # compute the x-axis
+  x_axis = x_axis[0:749] # get only the freqs we care about
+  fft = fft[0][0:749].real # get the magnitude only
+
+  # get the bands
+  delta = sum(fft[bands[0][0]:bands[0][1]])
+  theta = sum(fft[bands[1][0]:bands[1][1]])
+  alpha = sum(fft[bands[2][0]:bands[2][1]])
+  sigma = sum(fft[bands[3][0]:bands[3][1]])
+  beta = sum(fft[bands[4][0]:bands[4][1]])
+  gamma1 = sum(fft[bands[5][0]:bands[5][1]])
+  gamma2 = sum(fft[bands[6][0]:bands[6][1]])
+
+  # normalize
+  spectralSum = sum([delta, theta, alpha, sigma, beta, gamma1, gamma2])
+  delta = delta/spectralSum
+  theta = theta/spectralSum
+  alpha = alpha/spectralSum
+  sigma = sigma/spectralSum
+  beta = beta/spectralSum
+  gamma1 = gamma1/spectralSum
+  gamma2 = gamma2/spectralSum
+
+  # make it a string as CSV and write it
+  line = ', '.join(map(str, (time, delta, theta, alpha, beta, gamma)))
+  line += "\n"
+  output.write(line)
+
+output.close()
+
+# plt.plot(fft)
+# plt.show()
---
+++ scratch.py
@@ -0,0 +1,62 @@
+import mne
+import numpy as np
+import matplotlib.pyplot as plt
+# from visbrain import Sleep
+
+f = "/home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11.edf"
+annotation = "home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11.txt"
+exclude = ["ROC-LOC", "ECG1-ECG2", "DX1-DX2", "SX1-SX2", "SAO2", "HR", "PLETH", "STAT"] # don't use these channels
+bands = [(5,20), (21,45), (46,80), (81,125), (126,155), (156,200)]
+output = open("/home/roger/Documents/jacob/pieces/eoiayidbyII/data/n11/n11_analysis", "w")
+
+def normalize_complex_arr(a):
+  a_oo = a - a.real.min() - 1j*a.imag.min() # origin offsetted
+  return a_oo/np.abs(a_oo).max() # divide by the max magnitude
+
+raw = mne.io.read_raw_edf(f, exclude=exclude)
+# raw.plot(block=True, lowpass=40)
+
+overlap = 0.5
+seg_length = int(raw.info['sfreq'] * 2)
+advance = seg_length * overlap
+num_segs = int(raw.n_times/advance) # number of samples to advance per loop
+print (num_segs)
+
+time = (512 + advance) / raw.info['sfreq']
+
+# get the raw values into their own arary
+data = raw.get_data([0], 0, 0+seg_length)
+pad = (5120-len(data[0]))
+print (pad)
+data = np.lib.pad(data, pad_width=(0,pad), mode='constant', constant_values=0)
+data = mne.filter.filter_data(data, raw.info['sfreq'], l_freq=None, h_freq=50, fir_design='firwin')
+
+fft = np.fft.fft(data)
+fft = normalize_complex_arr(fft)
+x_axis = np.fft.fftfreq(5120, 1/raw.info['sfreq'])
+x_axis = x_axis[0:749]
+fft = fft[0][0:749].real
+# print (x_axis)
+
+# get the bands
+delta = sum(fft[bands[0][0]:bands[0][1]])
+theta = sum(fft[bands[1][0]:bands[1][1]])
+alpha = sum(fft[bands[2][0]:bands[2][1]])
+beta = sum(fft[bands[3][0]:bands[3][1]])
+gamma = sum(fft[bands[4][0]:bands[4][1]])
+
+spectralSum = sum([delta, theta, alpha, beta, gamma])
+delta = delta/spectralSum
+theta = theta/spectralSum
+alpha = alpha/spectralSum
+beta = beta/spectralSum
+gamma = gamma/spectralSum
+
+line = ', '.join(map(str, (time, delta, theta, alpha, beta, gamma)))
+line += "\n"
+output.write(line)
+
+output.close()
+
+# plt.plot(fft)
+# plt.show()
